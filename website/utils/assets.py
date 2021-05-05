71f90b1a4d3fb22de1148ea732ebc10de23805d5
Barrera <barreraalexander93@gmail.com>
2021-05-05 10:18:34 -0400

    updated gitignore

--- website/utils/assets.py
+++ website/utils/assets.py
@@ -7,17 +7,12 @@ bundles = {
         depends='scss/*.scss',
         output='gen/css/main.%(version)s.css'
     ),
-    'main_js':Bundle(
-        'scss/main.scss',
-        filters='libsass',
-        depends='scss/*.scss',
-        output='gen/css/main.%(version)s.css'
-    ),
-    'assets_js':Bundle(
-        'scss/main.scss',
-        filters='libsass',
-        depends='scss/*.scss',
-        output='gen/css/main.%(version)s.css'
+    'main_js': Bundle(
+        'js/scrolling_mod.js',
+        'js/planets_mod.js',
+        filters='jsmin',
+        depends='js/*.js',
+        output='gen/js/main.%(version)s.js'
     )
 }
 

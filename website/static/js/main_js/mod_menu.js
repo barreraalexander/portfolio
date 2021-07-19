e7a8570730b23be7448521eac047eed0219d5394
Barrera <barreraalexander93@gmail.com>
2021-07-19 14:54:41 -0400

    fixed mobile menu

--- website/static/js/main_js/mod_menu.js
+++ website/static/js/main_js/mod_menu.js
@@ -12,31 +12,29 @@ function mod_menu(){
         hidden_menu.classList.remove('apply_hide_menu')
         hidden_menu.classList.add('apply_show_menu')
         hidden_menu.dataset.menu_status = 'open'
-        window.addEventListener('click', check_target, false)
-
+        window.addEventListener('click', check_target, true)
+        
     } else if (menu_status=='open'){
-        window.removeEventListener('click', check_target, false)
-        menu_status = 'closed' 
+        alert('here')
+        hidden_menu.dataset.menu_status = 'closed'
+        hidden_menu.classList.remove('apply_show_menu')
+        hidden_menu.classList.add('apply_hide_menu')
+        window.removeEventListener('click', check_target, true)
     }
 }
 
 
 function check_target(event){
     var hidden_menu = document.querySelector('#hidden_nav')
-    var menu_status = hidden_menu.dataset.menu_status
+    // var menu_status = hidden_menu.dataset.menu_status
 
     if (event.target != hidden_menu &&
         event.target != nav_button &&
         event.target.parentNode != hidden_menu){
 
-        
+        console.log(hidden_menu)
+        mod_menu()
 
-        // hidden_menu.dataset.menu_status = 'closed'
-        
-        // hidden_menu.classList.remove('apply_show_menu')
-        // hidden_menu.classList.add('apply_hide_menu')
-        menu_status = 'closed'
-        window.removeEventListener('click', check_target)
 
     }
 }

2859948332500a670b9d40ddddcc200c58be9e38
Barrera <barreraalexander93@gmail.com>
2021-08-21 10:27:42 -0400

    added some metadata

--- website/blueprints/main/__init__.py
+++ website/blueprints/main/__init__.py
@@ -5,8 +5,15 @@ CONTACT_DICT = {
 }
 
 META_DICT = {
-    "description" : "",
-     "keywords" : "",
+    "description" : """
+                    Barrera Portfolio is a website used to
+                    showcase the web development experience
+                    of Alexander Barrera. He is a freelancing
+                    fullstack engineer from south Florida.
+                    """,
+     "keywords" : """
+                    fullstack engineer, web developer, programmer, web freelancer, web developer south florida, python engineer, database manager, coder, python flask, API manager, web designer  
+                  """,
     "domain" : "www.barrera-port.co",
     "domain_name" : "Barrera Portfolio"
 }

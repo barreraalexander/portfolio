91dbee456f9eb7c2115f6d9685fe61d4129b4850
barreraalexander <barreraalexander93@gmail.com>
2022-04-29 01:08:23 -0400

    update header

--- website/static/js/animations/hidden.js
+++ website/static/js/animations/hidden.js
@@ -1,14 +1,13 @@
 let hidden_section = document.querySelector('#hidden_section')
 
-let open_navigation_buttons = document.querySelectorAll('.open_navigation_menu')
-let close_navigation_buttons = document.querySelectorAll('.close_navigation_menu')
+let open_navigation = document.querySelector('#hamburger')
+let close_navigation = document.querySelector('#close_hidden_menu')
 
-let navigation_buttons = [...open_navigation_buttons, ...close_navigation_buttons]
+let navigation_buttons = [open_navigation, close_navigation]
 
 let hidden_navigation_tl = gsap.timeline({
     paused: true,
 })
-
 
 hidden_navigation_tl.to(
     hidden_section,
@@ -18,20 +17,23 @@ hidden_navigation_tl.to(
         filter: 'blur(0px)',
         display: 'flex',
         ease: Sine.easeInOut,
+        zIndex: '3000',
     }
 )
 
 for (let menu_button of navigation_buttons){
     menu_button.addEventListener('click', mod_navigation, false)
+    console.log(menu_button)
     // alert('here ')
 }
 
 function mod_navigation(event){
-    if (event.target.classList.contains('.open_navigation_menu')){
-        alert('here')
-        // hidden_navigation_tl.play()
+    // console.log(event.target.classList)
+    if (event.target==open_navigation){
+        // alert('here')
+        hidden_navigation_tl.play()
     } else {
-        // hidden_navigation_tl.reverse()
-        
+        hidden_navigation_tl.reverse()
+        // alert('not here')
     }
 }

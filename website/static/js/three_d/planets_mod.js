892df2bb279d4361084bad9982e8a9457fb8121e
barreraalexander <barreraalexander93@gmail.com>
2022-04-29 15:24:23 -0400

    updated loading

--- website/static/js/three_d/planets_mod.js
+++ website/static/js/three_d/planets_mod.js
@@ -48,52 +48,74 @@ if (night_ctnr){
     const particles_mesh = new THREE.Points(particles_geomtery, particle_material)
     scene.add(particles_mesh)
 
-    const moon_link = night_ctnr.dataset.moon_model
 
-    const moon_loader = new THREE.GLTFLoader();
+    function load_on_succession(){
+        const load_manager2 = new THREE.LoadingManager();
 
-    moon_loader.load(
-        moon_link,
-        function ( gltf ) {
+        const ship_link = night_ctnr.dataset.ship_model
 
-                moon = gltf.scene;
+        const ship_loader = new THREE.GLTFLoader();
+    
+        ship_loader.load(
+            ship_link,
+            function ( gltf ) {
+    
+                    ship = gltf.scene;
+                    ship.scale.set(.5, .5, .5)
+                    ship.position.set(4, 4, 15)
+                    scene.add( ship );
+                    alert('added')
+            },
+        )
+    
 
-                moon.position.set(0, 4, -15)
-                scene.add( moon );
-        },
-    )
 
-    const ship_link = night_ctnr.dataset.ship_model
+        const planet_link = night_ctnr.dataset.planet_model
 
-    const ship_loader = new THREE.GLTFLoader();
+        const planet_loader = new THREE.GLTFLoader();
+    
+    
+    
+        planet_loader.load(
+            planet_link,
+            function ( gltf ) {
+    
+                    planet = gltf.scene;
+                    planet.scale.set(8, 8, 8)
+                    planet.position.set(4, 18, -20)
+                    scene.add( planet );
+            },
+        )
+    }
 
-    ship_loader.load(
-        ship_link,
-        function ( gltf ) {
 
-                ship = gltf.scene;
-                ship.scale.set(.5, .5, .5)
-                ship.position.set(4, 4, 15)
-                scene.add( ship );
-        },
-    )
+    function load_on_width(){
+        if (window.innerWidth >= 767 - 1){
+            const load_manager1 = new THREE.LoadingManager();
 
-    const planet_link = night_ctnr.dataset.planet_model
+            // load_manager1.onLoad = function(){
+            //     load_on_succession()
+            // }
 
-    const planet_loader = new THREE.GLTFLoader();
+            const moon_link = night_ctnr.dataset.moon_model
 
-    planet_loader.load(
-        planet_link,
-        function ( gltf ) {
+            const moon_loader = new THREE.GLTFLoader(load_manager1);
 
-                planet = gltf.scene;
-                planet.scale.set(8, 8, 8)
-                planet.position.set(4, 18, -20)
-                scene.add( planet );
-        },
-    )
+            moon_loader.load(
+                moon_link,
+                function ( gltf ) {
 
-    document.addEventListener('mousemove', animateParticles)
+                        moon = gltf.scene;
+
+                        moon.position.set(0, 4, -15)
+                        scene.add( moon );
+                        load_on_succession()
+                },
+            )
+        }
+    }
+
+    load_on_width()
 
     let mouse_x = 0
     let mouse_y = 0
@@ -107,6 +129,7 @@ if (night_ctnr){
         renderer.setSize(window.innerWidth, window.innerHeight);
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix()
+        load_on_width()
 
     })
 
@@ -117,13 +140,28 @@ if (night_ctnr){
         let elasped_time = clock.getElapsedTime()
         
 
-        moon.rotation.y += 0.003;
+        try {
+            if (moon){
+                moon.rotation.y += 0.003;
 
-        planet.rotation.y += 0.002;
-        planet.rotation.x += 0.001;
-        
-        ship.rotation.y -= 0.003;
-        ship.rotation.x += 0.003;
+            }
+
+            if (planet){
+                planet.rotation.y += 0.002;
+                planet.rotation.x += 0.001;
+            }
+
+
+            if (ship){
+                ship.rotation.y -= 0.003;
+                ship.rotation.x += 0.003;
+
+            }
+
+        } catch (error) {
+            console.log('3d assets are still loading')
+        }
+
 
         particles_mesh.rotation.y = (.05 * elasped_time)
         particles_mesh.rotation.x = (.0009 * -mouse_y)
@@ -131,27 +169,27 @@ if (night_ctnr){
 
         
 
-        if (window.innerWidth > 1400){
-            moon.position.z = 16
-            point_light.position.z = 16
-            planet.position.set(12, 30, -25)
-        } else {
-            moon.position.z = 5
-            point_light.position.z = 5
-            planet.position.set(4, 18, -20)
+        // if (window.innerWidth > 1400){
+        //     moon.position.z = 16
+        //     point_light.position.z = 16
+        //     planet.position.set(12, 30, -25)
+        // } else {
+        //     moon.position.z = 5
+        //     point_light.position.z = 5
+        //     planet.position.set(4, 18, -20)
 
-        }
+        // }
 
-        if (elasped_time > 1 && elasped_time < 40){
-            ship.position.y += .01
-            ship.position.x -= .01
-            ship.position.z -= .01
+        // if (elasped_time > 1 && elasped_time < 40){
+        //     ship.position.y += .01
+        //     ship.position.x -= .01
+        //     ship.position.z -= .01
             
-        } else {
-            ship.position.y -= .01
-            ship.position.x += .01
-            ship.position.z += .01
-        }
+        // } else {
+        //     ship.position.y -= .01
+        //     ship.position.x += .01
+        //     ship.position.z += .01
+        // }
 
         renderer.domElement.id = 'globe_canvas';
         renderer.render(scene, camera)

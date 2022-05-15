ce8a459ddd23c38aeb94423b4f5e25ff13bf1803
barreraalexander <barreraalexander93@gmail.com>
2022-05-15 17:10:23 -0400

    update to code

--- website/components/pieces/skill_card.py
+++ website/components/pieces/skill_card.py
@@ -5,23 +5,27 @@ def component(skill):
     skills = {
         'web_dev' : {
             'title' : 'Design to Build',
-            'blurb' : 'Pixel perfect matching between design and build',
+            'blurb' : 'If you want to provide the design, the differnce between design and product will be nill. I take pride in pixel perfect matching.',
             'img_src' : url_for('static', filename='images/assets/icons/skills/web_dev.svg'),
+            'variant' : '1',
         },
         'db_management' : {
             'title' : 'Database Management',
-            'blurb' : 'Certified db manager, choose between ',
+            'blurb' : 'Certified db manager. Choose between mysql or postgres, generally hosted on AWS RDS.',
             'img_src' : url_for('static', filename='images/assets/icons/skills/db_management.svg'),
+            'variant' : '2', 
         },
         'api_dev' : {
             'title' : 'API Development',
-            'blurb' : 'Professional level ',
+            'blurb' : 'Professional level API development, complete with correct status codes, and infiite scalabilty. The ORM can be pydantic, or graphql.',
             'img_src' : url_for('static', filename='images/assets/icons/skills/api_dev.svg'),
+            'variant' : '1', 
         },
         'server' : {
             'title' : 'Elastic Cloud Server',
             'blurb' : 'Deployment on the server is so 2014. Take advantage of the seemless scalabaility of elastic servers that grow and shrink with your traffic.',
             'img_src' : url_for('static', filename='images/assets/icons/skills/server.svg'),
+            'variant' : '2', 
         },
     }
 
@@ -29,18 +33,16 @@ def component(skill):
 
     dots = [
             """
-            <div class="dot">
+            <div class="dot apply_shift_dot">
             </div>
-    
-    
             """
-            for i in range(300)
+            for i in range(200)
         ]
 
 
     return Markup(f"""
     <div class="skill_card">
-        <div class="dotted_bg_ctnr">
+        <div class="dotted_bg_ctnr" data-variant="{chosen_skill.get('variant')}">
             {unpack_elems(dots)}
         </div>
         <div class="skill_card_contents">

247e4c09ab60bde9954cbf1864c761f447aac7ac
barreraalexander <barreraalexander93@gmail.com>
2022-05-26 12:50:44 -0400

    update

--- website/components/sections/minimalism.py
+++ website/components/sections/minimalism.py
@@ -1,10 +1,20 @@
 from flask import Markup
+from website.components.pieces.button_like import component as button_like
 
 def component():
     return Markup(f"""
     <section id="minimalism_section">
-        <h1>
-            Minimalism
-        </h1>
+        <div class="hero_text_ctnr">
+            <h1>
+                Minimal
+            </h1>
+            <p class="description">
+                The go-to style for IT companies, 
+            </p>
+            <div class="buttons_ctnr">
+                {button_like('call')}
+                {button_like('resume')}
+            </div>
+        </div>
     </section>
     """)

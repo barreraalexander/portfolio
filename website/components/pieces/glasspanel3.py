adeb18ac58ba7a83b49119dfc1c1ae756e6ead44
barreraalexander <barreraalexander93@gmail.com>
2022-05-31 00:26:25 -0400

    updates to code

---
+++ website/components/pieces/glasspanel3.py
@@ -0,0 +1,73 @@
+from flask import Markup
+from random import choice
+from website.utils.webdata import unpack_elems
+from datetime import datetime, timedelta
+
+def component():
+    posts_data = {
+        'post1' : {
+            'title' : "How Adam and Eve Cooked Eggs in the Iron Age",
+            'blurb' : "Commodo sint cupidatat commodo et minim amet non ipsum irure ut pariatur adipisicing non. Qui esse dolor reprehenderit adipisicing sint minim sint. Non commodo do reprehenderit laborum est veniam excepteur incididunt sit ad ullamco laborum Lorem et. Laborum reprehenderit amet culpa duis aute in magna esse voluptate velit quis anim. Minim sit sint dolore sit ipsum commodo duis velit anim Lorem.",
+            'posted' : (datetime.now() - timedelta(days=3, hours=5, minutes=5)).strftime("%b %d, %Y | %I:%M %p"),
+            'tags' : ['history', 'funny', 'top post']
+        }, 
+        'post2' : {
+            'title' : "Between the Queen and I",
+            'blurb' : "Commodo sint cupidatat commodo et minim amet non ipsum irure ut pariatur adipisicing non. Qui esse dolor reprehenderit adipisicing sint minim sint. Non commodo do reprehenderit laborum est veniam excepteur incididunt sit ad ullamco laborum Lorem et. Laborum reprehenderit amet culpa duis aute in magna esse voluptate velit quis anim. Minim sit sint dolore sit ipsum commodo duis velit anim Lorem.",
+            'posted' : (datetime.now() - timedelta(days=7, hours=2, minutes=17)).strftime("%b %d, %Y | %I:%M %p"),
+            'tags' : ['art', 'sad', 'trending']
+        }, 
+        'post3' : {
+            'title' : "The Twins Miquella and Malenia",
+            'blurb' : "Commodo sint cupidatat commodo et minim amet non ipsum irure ut pariatur adipisicing non. Qui esse dolor reprehenderit adipisicing sint minim sint. Non commodo do reprehenderit laborum est veniam excepteur incididunt sit ad ullamco laborum Lorem et. Laborum reprehenderit amet culpa duis aute in magna esse voluptate velit quis anim. Minim sit sint dolore sit ipsum commodo duis velit anim Lorem.",
+            'posted' : (datetime.now() - timedelta(days=7, hours=8, minutes=25)).strftime("%b %d, %Y | %I:%M %p"),
+            'tags' : ['history', 'funny', 'top post']
+        }, 
+    }
+
+    def make_post(post_dict):
+        return Markup(f""" 
+        <div>
+            <div class="post">
+                <h4>
+                    {post_dict.get('title')}
+                </h4>
+                <p class="posted">
+                    {post_dict.get('posted')}
+                </p>
+                <div class="tags_ctnr">
+                    <p>
+                        #{post_dict.get('tags')[0]}
+                    </p>
+                    <p>
+                        #{post_dict.get('tags')[1]}
+                    </p>
+                    <p>
+                        #{post_dict.get('tags')[2]}
+                    </p>
+                </div>
+                <p class="blurb">
+                    {post_dict.get('blurb')}
+                </p>
+            </div>
+        </div>
+
+        """)
+
+    return Markup(f"""
+    <div class="panel glass panel3">
+        <h2>
+            Structured Information
+        </h2>
+        <p>
+            Works beautifully with panels of glass. Everything is separated and sections are plainly labeled. Glass style is great for websites that have to display a lot of information.
+        </p>
+        <div class="postlist_ctnr">
+            {make_post(posts_data.get('post1'))}
+            <hr>
+            {make_post(posts_data.get('post2'))}
+            <hr>
+            {make_post(posts_data.get('post3'))}
+        </div>
+    </div>
+    """)
--- website/components/sections/glassmin.py
+++ website/components/sections/glassmin.py
@@ -2,6 +2,10 @@ from flask import Markup, url_for
 from website.components.pieces.button_like import component as button_like
 from datetime import datetime
 
+from website.components.pieces.glasspanel1 import component as gp1
+from website.components.pieces.glasspanel2 import component as gp2
+from website.components.pieces.glasspanel3 import component as gp3
+
 def component():
     return Markup(f"""
     <section id="glassmin_section">
@@ -10,87 +14,11 @@ def component():
         </h1>
         
         <div class="panels_ctnr">
-
             <div class="desktop_sidepanel">
-
-                <div class="panel glass panel1">
-                    <h2>
-                        My Profile
-                    </h2>
-                    <div class="profile_ctnr">
-                        <img
-                            class="profile_img"
-                            src="{ url_for('static', filename='images/assets/barrera_cutout.png') }"
-                        >
-                        <div class="profile_details">
-                            <h3>
-                                Alexander Barrera
-                            </h3>
-                            <small>
-                                Hollywood, FL
-                            </small>
-                            <small>
-                                {datetime.now().strftime("%I:%M %p")}
-                            </small>
-                            {button_like('available')}
-                        </div>
-                    </div>
-                    <p>
-                        Profile sections would look something like this. Often pushed off to the right and above non-essential information in desktop mode, and sitting at the top of the stack in mobile mode.
-                    </p>
-                </div>
-
-
-                <div class="panel glass panel2">
-                    <h2>
-                        Nonessential Information
-                    </h2>
-                    <p>
-                        Is pushed off to the right. This would often include the archive section of a news website or blog. On social media sites, the nonessential section would store advertisements, suggested friends and groups. 
-                    </p>
-                    <ul>
-                        <li>
-                            March 2022
-                        </li>
-                        <li>
-                            February 2022
-                        </li>
-                        <li>
-                            January 2022
-                        </li>
-                        <li>
-                            December 2021
-                        </li>
-                        <li>
-                            November 2021
-                        </li>
-                        <li>
-                            October 2021
-                        </li>
-                        <li>
-                            September 2021
-                        </li>
-                        <li>
-                            August 2021
-                        </li>
-                        <li>
-                            July 2021
-                        </li>
-                    </ul>
-                </div>
+                {gp1()}
+                {gp2()}
             </div>
-
-            <div class="panel glass panel3">
-                <h2>
-                    Structured Information
-                </h2>
-                <p>
-                    Works beautifully with panels of glass. Everything is separated and sections are plainly labeled. Glass style is great for websites that have to display a lot of information.
-                </p>
-            </div>
-
-
-
+            {gp3()}
         </div>
 
     </section>

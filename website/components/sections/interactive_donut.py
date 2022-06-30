782b9d43ca90fc4511f65bd1a884427d37aca701
barreraalexander <barreraalexander93@gmail.com>
2022-06-30 19:57:12 -0400

    updates to base

--- website/components/sections/interactive_donut.py
+++ website/components/sections/interactive_donut.py
@@ -2,23 +2,33 @@ from flask import Markup, url_for
 
 def component():
     three_d_script = url_for('static', filename='js/three_d/interactive_donut.js')
-    
+    # texture1 = url_for('static', filename='images/assets/three_d/texture5.jpg')
+    texture1 = url_for('static', filename='images/assets/three_d/sprinkle_texture.png')
+    # texture1 = url_for('static', filename='images/assets/three_d/bubble_texture.png')
+
+
+    moon_model = url_for('static', filename='images/assets/three_d/moon.glb')
+    ship_model = url_for('static', filename='images/assets/three_d/ship.glb')
+    planet_model = url_for('static', filename='images/assets/three_d/planet.glb')
+
+
+
     return Markup(f"""
     <section id="interactive_donut_section">
         <h1>
             Interactive 3d Objects
         </h1>
         <div class="interactive_panel">
-            <div id="donut_ctnr">
+            <div id="donut_ctnr" data-texture1="{texture1}" data-model1="{planet_model}" data-model2="{ship_model}">
             </div>
             <div id="controls_ctnr">
                 <div class="movement_controls">
-                    <div id="backward" class="control">
+                    <div id="forward" class="control">
                         <p>
                             ⬆
                         </p>
                     </div>
-                    <div id="forward" class="control">
+                    <div id="backward" class="control">
                         <p>
                             ⬇
                         </p>
--- website/static/js/three_d/interactive_donut.js
+++ website/static/js/three_d/interactive_donut.js
@@ -1,6 +1,14 @@
+var donut_section = document.querySelector('#interactive_donut_section')
 var donut_ctnr = document.querySelector('#donut_ctnr')
 
 if (donut_ctnr){
+    let texture_path = donut_ctnr.dataset.texture1
+    const donut_textureLoader = new THREE.TextureLoader()
+    const bubbleTexture = donut_textureLoader.load(texture_path)
+
+
+
+
     var donut_scene = new THREE.Scene();
     var donut_camera = new THREE.PerspectiveCamera(
         75,
@@ -10,26 +18,62 @@ if (donut_ctnr){
         0.1,
         1000
     )
-
-    donut_camera.position.z = 30
+    donut_camera.position.z = 10
 
     var donut_renderer = new THREE.WebGLRenderer({
         antialias: true
     })
 
-    // donut_renderer.setClearColor(new THREE.Color('#fff'))
-    // donut_renderer.setSize(window.innerWidth, window.innerHeight)
+
     donut_renderer.setSize(donut_ctnr.offsetWidth, donut_ctnr.offsetHeight)
 
     donut_ctnr.appendChild(donut_renderer.domElement)
 
     const donut_hemi_light = new THREE.HemisphereLight (0xFFFFFF, 1, 2)
-    // donut_scene.add(donut_hemi_light)
+    donut_scene.add(donut_hemi_light)
+
+    // const donut_geo = new THREE.TorusGeometry(10, 1, 100, 200);
+    const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
+    const sphere_geo = new THREE.SphereBufferGeometry(5, 64, 64);
+    
+    const donut_mat = new THREE.MeshStandardMaterial({
+        color: new THREE.Color('#DEB887'),
+        // wireframe: true,
+    });
+
+    const sphere_mat = new THREE.MeshNormalMaterial({
+        // color: new THREE.Color('#DEB887'),
+        // wireframe: true,
+    });
+
+
+    donut_mat.normalMap = bubbleTexture;
+    sphere_mat.normalMap = bubbleTexture;
+    // donut_mat.metalness = 0.1
+    // donut_mat.roughtness = 0.3
+
+
+    const donut_mesh = new THREE.Mesh(donut_geo, donut_mat)
+    const sphere_mesh = new THREE.Mesh(sphere_geo, sphere_mat)
+
+
+    donut_mesh.position.set(0, 0, -30)
+    sphere_mesh.position.set(0, 0, -60)
+
+
+
+    // console.log(texture_path)
+
+
+
+
+    // donut_scene.add(donut_mesh);
+    // donut_scene.add(sphere_mesh);
+
     
 
     const particles_geomtery = new THREE.BufferGeometry;
     const particles_count = 10000/8;
-
     const position_array = new Float32Array(particles_count * 3)
 
     for (let i = 0; i < particles_count * 3; i++){
@@ -52,15 +96,127 @@ if (donut_ctnr){
         donut_renderer.setSize(donut_ctnr.offsetWidth, donut_ctnr.offsetHeight);
         donut_camera.aspect = donut_ctnr.offsetWidth / donut_ctnr.offsetHeight;
         donut_camera.updateProjectionMatrix()
-        
+
     })
+
+
+    let arrow_controls = donut_section.querySelectorAll('.control')
+    for (let control of arrow_controls){
+        control.addEventListener('click', handleMovement, false)
+    }
+
+
+    let changer_controls = donut_section.querySelectorAll('.changer')
+    for (let control of changer_controls){
+        control.addEventListener('click', handleChanger, false)
+    }
+
+    function handleMovement(event){
+        let direction = event.target.id
+        
+        var max_z = 30
+        var min_z = -12
+        // var max_z = 0
+
+        
+
+
+        if (direction=='forward'){
+            donut_camera.position.z -= 1
+            return     
+        }
+        
+        if (direction=='backward'){
+            if (donut_camera.position.z < max_z){
+                donut_camera.position.z += 1
+            } 
+            return     
+        }
+        
+        if (direction=='left'){
+            donut_camera.position.x -= 1       
+            return     
+        }
+        
+        if (direction=='right'){
+            donut_camera.position.x += 1       
+            return     
+        }
+    }
+
+    var color_selector = 0
+    function handleChanger(event){
+        let command = event.target.id
+        if (command=="change_mesh"){
+            // alert('mesh')
+            // donut_mesh.
+            donut_mesh.material.color.setHex( 0xffffff )
+            
+            return
+        }
+        if (command=="change_color"){
+            // alert('color')
+            
+            donut_mesh.material.color.setHex( 0xffffff )
+
+
+            return
+        }
+    }
+
+
+    function load_moon(){
+        const moon_link = donut_ctnr.dataset.model1
+        const moon_loader = new THREE.GLTFLoader();
+        moon_loader.load(
+            moon_link,
+            function ( gltf ) {
+                moon = gltf.scene;
+                moon.position.set(0, 0, 6);
+                donut_scene.add( moon );
+            },
+        )
+    }
+    load_moon()
+
+
+    function load_ship(){
+        const ship_link = donut_ctnr.dataset.model2
+        const ship_loader = new THREE.GLTFLoader();
+        ship_loader.load(
+            ship_link,
+            function ( gltf ) {
+                ship = gltf.scene;
+                ship.scale.set(.25, .25, .25)
+                ship.position.set(1, 4, 1)
+                donut_scene.add( ship );
+            },
+        )
+    }
+    load_ship()
 
 
 
     var donut_render = function(){
         requestAnimationFrame(donut_render)
 
-        // donut_renderer.domElement.id = 'donut_canvas'
+
+        sphere_mesh.rotation.x += .002;
+        sphere_mesh.rotation.y += .008;
+        
+        
+        try {
+            if (moon){
+                moon.rotation.x += .002;
+                moon.rotation.y += .005;
+            }
+            if (ship){
+                ship.rotation.y += .002
+            }
+        } catch (error) {
+            
+        }
+
         donut_renderer.render(donut_scene, donut_camera)
     }
 

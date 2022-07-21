e8fc7591349ca038fd10bd498756b466378f325a
barreraalexander <barreraalexander93@gmail.com>
2022-07-20 20:52:22 -0400

    new configuration works

--- website/__init__.py
+++ website/__init__.py
@@ -1,27 +1,23 @@
 from flask import Flask
 from flask_assets import Environment
 from flask_mail import Mail
-# from flask_caching import Cache
 
-from website.config import Config, cacheConfig
+from website.config import settings, Settings
 from website.utils.assets import bundles
 
 
 assets = Environment ()
-# cache = Cache(config=cacheConfig)
 mail = Mail()
 
 from website.utils.assets import bundles
 
-def create_app (config_class=Config):
+def create_app (config_class=Settings):
     app = Flask (__name__)
-    app.config.from_object(Config)
-    
+    app.config.from_object(settings)
+
     assets.init_app(app)
     assets.register(bundles)
     mail.init_app(app)
-    # cache.init_app(app)
-
     
     from website.blueprints.main.routes import main
     from website.blueprints.api.routes import api
--- website/config.py
+++ website/config.py
@@ -1,30 +1,44 @@
-import json
-from os import path
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    RECAPTCHA_PUBLIC_KEY: str
+    RECAPTCHA_SECRET_KEY: str
+    EMAIL_USER: str
+    EMAIL_PW: str
+
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
+
+# import json
+# from os import path
 
 
-local_config = path.expanduser('~/etc/portfolio_config.json')
-launch_config = '/etc/portfolio_config.json'
+# local_config = path.expanduser('~/etc/portfolio_config.json')
+# launch_config = '/etc/portfolio_config.json'
 
-with open(local_config) as config_file:
-    config = json.load(config_file)
+# with open(local_config) as config_file:
+#     config = json.load(config_file)
 
 
-class Config:
-    SECRET_KEY = config.get('SECRET_KEY')
+# class Config:
+#     SECRET_KEY = config.get('SECRET_KEY')
 
-    RECAPTCHA_PUBLIC_KEY = config.get('RECAPTCHA_PUBLIC_KEY')
-    RECAPTCHA_PRIVATE_KEY = config.get('RECAPTCHA_SECRET_KEY')
+#     RECAPTCHA_PUBLIC_KEY = config.get('RECAPTCHA_PUBLIC_KEY')
+#     RECAPTCHA_PRIVATE_KEY = config.get('RECAPTCHA_SECRET_KEY')
 
-    MAIL_USERNAME = config.get('EMAIL_USER')
-    MAIL_DEFAULT_SENDER = config.get('EMAIL_USER')
-    MAIL_PASSWORD = config.get('EMAIL_PW')
+#     MAIL_USERNAME = config.get('EMAIL_USER')
+#     MAIL_DEFAULT_SENDER = config.get('EMAIL_USER')
+#     MAIL_PASSWORD = config.get('EMAIL_PW')
 
-    MAIL_PORT = 587
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_USE_TLS = True
+#     MAIL_PORT = 587
+#     MAIL_SERVER = 'smtp.gmail.com'
+#     MAIL_USE_TLS = True
 
-cacheConfig = {
-    "DEBUG" : True,
-    "CACHE_TYPE" : "simple",
-    "CACHE_DEFAULT_TIMEOUT" : 300
-}
+# cacheConfig = {
+#     "DEBUG" : True,
+#     "CACHE_TYPE" : "simple",
+#     "CACHE_DEFAULT_TIMEOUT" : 300
+# }

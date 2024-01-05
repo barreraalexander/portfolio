32387d8f94cc30af2e06a11d834e0fde18af9c96
Alexander Barrera <barreraalexander93@gmail.com>
2024-01-05 14:56:25 -0500

    rebundled all js, everything is working perfectly

--- website/static/js/three_d/SpaceScene.js
+++
@@ -1,6 +0,0 @@
-export default class SpaceScene extends THREE.Scene
-{
-    
-    // alert('here')
-    // r = new 
-}
--- website/utils/assets.py
+++ website/utils/assets.py
@@ -14,18 +14,34 @@ bundles = {
         ],
         output='gen/css/main.%(version)s.css'
     ),
-    'main_js':Bundle(
+    'animations_js':Bundle(
         'js/animations/contact_modal.js',
+        'js/animations/dramatic_h1.js',
+        'js/animations/experience.js',
+        'js/animations/glassmin.js',
+        'js/animations/hero.js',
+        'js/animations/hidden.js',
+        'js/animations/minimalism.js',
+        'js/animations/neubrutalism.js',
+        'js/animations/resume_like.js',
+        'js/animations/shifting_dots.js',
+        'js/animations/skill_card.js',
+        'js/animations/story.js',
         filters='jsmin',
         depends=[
             'js/animations/*.js',
-            # 'scss/base/*.scss',
-            # 'scss/pages/*.scss',
-            # 'scss/components/navbars/*.scss',
-            # 'scss/components/pieces/*.scss',
-            # 'scss/components/sections/*.scss',
         ],
-        output='gen/js/main.%(version)s.js'
+        output='gen/js/animations.%(version)s.js'
+    ),
+    'three_d_js':Bundle(
+        # 'js/three_d/interactive_donut.js',
+        'js/three_d/planets_mod.js',
+        # 'js/three_d/rain_scape.js',
+        filters='jsmin',
+        depends=[
+            'js/three_d/*.js',
+        ],
+        output='gen/js/three_d.%(version)s.js'
     ),
 }
 

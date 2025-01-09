9b0227eaddf391fbede6b7ebe6954618ccc770ab
Alexander Barrera <barreraalexander93@gmail.com>
2025-01-09 14:38:40 -0500

    update

--- website/static/js/three_modules/planets/init.js
+++ website/static/js/three_modules/planets/init.js
@@ -121,8 +121,8 @@ if (night_ctnr){
             }
 
             if (window.innerWidth > 1399){
-                controls.enableRotate = true;
-                controls.update()
+                // controls.enableRotate = true;
+                // controls.update()
 
                 if (moon){
                     moon.position.set = (1, 8, 8)
@@ -133,8 +133,8 @@ if (night_ctnr){
                 }
 
             } else {
-                controls.enableRotate = false;
-                controls.update()
+                // controls.enableRotate = false;
+                // controls.update()
 
                 if (moon){
                     moon.position.set = (0, 100, 0)
